83f262cdce9e1bdf050f640a1bcc5bccb66d4fd1
PROCuuuba <barkalov.ilia@gmail.com>
2024-03-12 21:54:47 +0300

    Добавьте файлы проекта.

---
+++ ZooShop/Category.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooShop
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+
+        public Category(int categoryId, string name)
+        {
+            CategoryId = categoryId;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
